44ac326f5bbcb082615e327ecd5bc3e002e424eb
Kishwar <kishwar.shafin@gmail.com>
2019-03-04 23:03:32 -0800

    swap parameters :fire:

--- train.py
+++ train.py
@@ -33,8 +33,8 @@ class TrainModule:
         self.stats_dir = stats_dir
         self.hidden_size = 256
         self.gru_layers = 3
-        self.learning_rate = 5.1280939340415364e-05
-        self.weight_decay = 0.00019033063058448077
+        self.learning_rate = 0.00019033063058448077
+        self.weight_decay = 5.1280939340415364e-05
 
     def train_model(self):
         # train a model
