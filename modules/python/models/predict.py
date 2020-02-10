b21f4f8a8ceb7c43b269c30110b220a60ba9aaab
Kishwar <kishwar.shafin@gmail.com>
2020-02-10 11:58:41 -0800

    debug onnx :fire:

--- modules/python/models/predict.py
+++ modules/python/models/predict.py
@@ -80,7 +80,7 @@ def predict(test_file, output_filename, model_path, batch_size, threads, num_wor
         sys.stderr.write("INFO: LOADING ONNX MODEL\n")
         onnx_model = onnx.load(model_path + ".onnx")
         sys.stderr.write("INFO: CHECKING ONNX MODEL\n")
-        onnx.checker.check_model(onnx_model)
+        print(onnx.checker.check_model(onnx_model))
         sys.stderr.write("INFO: ONNX SESSION INITIALIZING\n")
         ort_session = onnxruntime.InferenceSession(model_path + ".onnx")
 
