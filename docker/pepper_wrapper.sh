a00a2f29a01cc32ba1e996b8a6b24c3a5403f718
Kishwar <kishwar.shafin@gmail.com>
2020-02-09 18:18:05 -0800

    dockerfile update :fire:

--- docker/pepper_wrapper.sh
+++ docker/pepper_wrapper.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-set -e
-
-# Fix ownership of output files
-finish() {
-    # Fix ownership of output files
-    user_id=$(stat -c '%u:%g' /data)
-    chown -R ${user_id} /data
-}
-trap finish EXIT
-
-# RUN HELEN SCRIPT
+# RUN PEPPER SCRIPT
 python3 /opt/pepper/$@
