314cb92e8596091ccfefa8e816ec4e8b4a17040b
Kishwar <kishwar.shafin@gmail.com>
2020-02-10 19:57:42 -0800

    htslib fix :fire:

--- modules/htslib.cmake
+++ modules/htslib.cmake
@@ -9,7 +9,7 @@ ExternalProject_Add(htslib
         URL https://github.com/samtools/htslib/releases/download/1.9/htslib-1.9.tar.bz2
         PREFIX ${htslib_PREFIX}
         UPDATE_COMMAND ""
-        CONFIGURE_COMMAND autoconf && ./configure --prefix=${CMAKE_BINARY_DIR}/htslib --disable-lzma --disable-s3 --disable-plugins --disable-bz2 --disable-libcurl
+        CONFIGURE_COMMAND autoconf && ./configure --prefix=${CMAKE_BINARY_DIR}/htslib
         BUILD_COMMAND make CFLAGS=${CMAKE_C_FLAGS}
         INSTALL_COMMAND "")
 ExternalProject_Get_Property(htslib SOURCE_DIR)
