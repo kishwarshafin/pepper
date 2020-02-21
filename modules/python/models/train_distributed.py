daeb2c7fca39f5a2be35ae73bf5e9f9faaed5151
Kishwar <kishwar.shafin@gmail.com>
2020-02-20 22:40:23 -0800

    test distributed training :fire:

--- modules/python/models/train_distributed.py
+++ modules/python/models/train_distributed.py
@@ -52,9 +52,10 @@ def save_best_model(transducer_model, model_optimizer, hidden_size, layers, epoc
 
 
 def train(train_file, test_file, batch_size, epoch_limit, gpu_mode, num_workers, retrain_model,
-          retrain_model_path, gru_layers, hidden_size, lr, decay, model_dir, stats_dir, train_mode):
+          retrain_model_path, gru_layers, hidden_size, lr, decay, model_dir, stats_dir, train_mode,
+          world_size, rank, device_id):
 
-    if train_mode is True:
+    if train_mode is True and rank == 0:
         train_loss_logger = open(stats_dir + "train_loss.csv", 'w')
         test_loss_logger = open(stats_dir + "test_loss.csv", 'w')
         confusion_matrix_logger = open(stats_dir + "confusion_matrix.txt", 'w')
@@ -63,13 +64,27 @@ def train(train_file, test_file, batch_size, epoch_limit, gpu_mode, num_workers,
         test_loss_logger = None
         confusion_matrix_logger = None
 
-    sys.stderr.write(TextColor.PURPLE + 'Loading data\n' + TextColor.END)
+    torch.cuda.set_device(device_id)
+
+    if rank == 0:
+        sys.stderr.write(TextColor.PURPLE + 'Loading data\n' + TextColor.END)
+
     train_data_set = SequenceDataset(train_file)
-    train_loader = DataLoader(train_data_set,
-                              batch_size=batch_size,
-                              shuffle=True,
-                              num_workers=num_workers,
-                              pin_memory=gpu_mode)
+
+    train_sampler = torch.utils.data.distributed.DistributedSampler(
+        train_data_set,
+        num_replicas=world_size,
+        rank=rank
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_data_set,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=0,
+        pin_memory=True,
+        sampler=train_sampler)
+
     num_classes = ImageSizeOptions.TOTAL_LABELS
 
     if retrain_model is True:
@@ -108,14 +123,15 @@ def train(train_file, test_file, batch_size, epoch_limit, gpu_mode, num_workers,
         sys.stderr.write(TextColor.GREEN + "INFO: OPTIMIZER LOADED\n" + TextColor.END)
 
     if gpu_mode:
-        transducer_model = transducer_model.cuda()
+        transducer_model = transducer_model.to(device_id)
+        transducer_model = nn.parallel.DistributedDataParallel(transducer_model, device_ids=[device_id])
 
     class_weights = torch.Tensor(CLASS_WEIGHTS)
     # Loss
     criterion = nn.CrossEntropyLoss(class_weights)
 
     if gpu_mode is True:
-        criterion = criterion.cuda()
+        criterion = criterion.to(device_id)
 
     start_epoch = prev_ite
 
@@ -128,69 +144,81 @@ def train(train_file, test_file, batch_size, epoch_limit, gpu_mode, num_workers,
     for epoch in range(start_epoch, epoch_limit, 1):
         total_loss = 0
         total_images = 0
-        sys.stderr.write(TextColor.BLUE + 'Train epoch: ' + str(epoch + 1) + "\n")
+        if rank == 0:
+            sys.stderr.write(TextColor.BLUE + 'Train epoch: ' + str(epoch + 1) + "\n")
         # make sure the model is in train mode. BN is different in train and eval.
 
         batch_no = 1
-        with tqdm(total=len(train_loader), desc='Loss', leave=True, ncols=100) as progress_bar:
-            transducer_model.train()
-            for images, labels in train_loader:
-                labels = labels.type(torch.LongTensor)
-                images = images.type(torch.FloatTensor)
+        if rank == 0:
+            progress_bar = tqdm(
+                total=len(train_loader),
+                ncols=100,
+                leave=False,
+                position=rank,
+                desc="Loss: ",
+            )
+        else:
+            progress_bar = None
 
-                if gpu_mode:
-                    # encoder_hidden = encoder_hidden.cuda()
-                    images = images.cuda()
-                    labels = labels.cuda()
+        transducer_model.train()
+        for images, labels in train_loader:
+            labels = labels.type(torch.LongTensor)
+            images = images.type(torch.FloatTensor)
+            if gpu_mode:
+                images = images.to(device_id)
+                labels = labels.to(device_id)
 
-                hidden = torch.zeros(images.size(0), 2 * TrainOptions.GRU_LAYERS, TrainOptions.HIDDEN_SIZE)
+            hidden = torch.zeros(images.size(0), 2 * TrainOptions.GRU_LAYERS, TrainOptions.HIDDEN_SIZE)
 
-                if gpu_mode:
-                    hidden = hidden.cuda()
+            if gpu_mode:
+                hidden = hidden.to(device_id)
 
-                for i in range(0, ImageSizeOptions.SEQ_LENGTH, TrainOptions.WINDOW_JUMP):
-                    model_optimizer.zero_grad()
-                    if i + TrainOptions.TRAIN_WINDOW > ImageSizeOptions.SEQ_LENGTH:
-                        break
+            for i in range(0, ImageSizeOptions.SEQ_LENGTH, TrainOptions.WINDOW_JUMP):
+                model_optimizer.zero_grad()
 
-                    image_chunk = images[:, i:i+TrainOptions.TRAIN_WINDOW]
-                    label_chunk = labels[:, i:i+TrainOptions.TRAIN_WINDOW]
+                if i + TrainOptions.TRAIN_WINDOW > ImageSizeOptions.SEQ_LENGTH:
+                    break
 
-                    output_, hidden = transducer_model(image_chunk, hidden)
+                image_chunk = images[:, i:i+TrainOptions.TRAIN_WINDOW]
+                label_chunk = labels[:, i:i+TrainOptions.TRAIN_WINDOW]
 
-                    loss = criterion(output_.contiguous().view(-1, num_classes), label_chunk.contiguous().view(-1))
+                output_, hidden = transducer_model(image_chunk, hidden)
 
-                    loss.backward()
-                    model_optimizer.step()
+                loss = criterion(output_.contiguous().view(-1, num_classes), label_chunk.contiguous().view(-1))
+                loss.backward()
 
-                    total_loss += loss.item()
-                    total_images += image_chunk.size(0)
+                model_optimizer.step()
+                total_loss += loss.item()
+                total_images += image_chunk.size(0)
+                hidden = hidden.detach()
 
-                    hidden = hidden.detach()
+            # update the progress bar
+            avg_loss = (total_loss / total_images) if total_images else 0
 
-                # update the progress bar
-                avg_loss = (total_loss / total_images) if total_images else 0
+            if train_mode is True and rank == 0:
+                train_loss_logger.write(str(epoch + 1) + "," + str(batch_no) + "," + str(avg_loss) + "\n")
+
+            if rank == 0:
                 progress_bar.set_description("Loss: " + str(avg_loss))
-
-                if train_mode is True:
-                    train_loss_logger.write(str(epoch + 1) + "," + str(batch_no) + "," + str(avg_loss) + "\n")
                 progress_bar.refresh()
                 progress_bar.update(1)
+                progress_bar.close()
                 batch_no += 1
 
-            progress_bar.close()
+        dist.barrier()
 
-        stats_dictioanry = test(test_file, batch_size, gpu_mode, transducer_model, num_workers,
-                                gru_layers, hidden_size, num_classes=ImageSizeOptions.TOTAL_LABELS)
-        stats['loss'] = stats_dictioanry['loss']
-        stats['accuracy'] = stats_dictioanry['accuracy']
-        stats['loss_epoch'].append((epoch, stats_dictioanry['loss']))
-        stats['accuracy_epoch'].append((epoch, stats_dictioanry['accuracy']))
+        if rank == 0:
+            stats_dictioanry = test(test_file, batch_size, gpu_mode, transducer_model, num_workers,
+                                    gru_layers, hidden_size, num_classes=ImageSizeOptions.TOTAL_LABELS)
+            stats['loss'] = stats_dictioanry['loss']
+            stats['accuracy'] = stats_dictioanry['accuracy']
+            stats['loss_epoch'].append((epoch, stats_dictioanry['loss']))
+            stats['accuracy_epoch'].append((epoch, stats_dictioanry['accuracy']))
 
         lr_scheduler.step(stats['loss'])
 
         # update the loggers
-        if train_mode is True:
+        if train_mode is True and rank == 0:
             # save the model after each epoch
             # encoder_model, decoder_model, encoder_optimizer, decoder_optimizer, hidden_size, layers, epoch,
             # file_name
@@ -202,13 +230,14 @@ def train(train_file, test_file, batch_size, epoch_limit, gpu_mode, num_workers,
             train_loss_logger.flush()
             test_loss_logger.flush()
             confusion_matrix_logger.flush()
-        else:
+        elif train_mode is False:
             # this setup is for hyperband
             if epoch + 1 >= 10 and stats['accuracy'] < 98:
                 sys.stderr.write(TextColor.PURPLE + 'EARLY STOPPING AS THE MODEL NOT DOING WELL\n' + TextColor.END)
                 return transducer_model, model_optimizer, stats
 
-    sys.stderr.write(TextColor.PURPLE + 'Finished training\n' + TextColor.END)
+    if rank == 0:
+        sys.stderr.write(TextColor.PURPLE + 'Finished training\n' + TextColor.END)
 
     return transducer_model, model_optimizer, stats
 
@@ -217,14 +246,16 @@ def cleanup():
     dist.destroy_process_group()
 
 
-def setup(rank, device_ids, args, all_input_files):
+def setup(rank, device_ids, args):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
 
     # initialize the process group
     dist.init_process_group("gloo", rank=rank, world_size=len(device_ids))
 
-    filepath, output_filepath, model_path, batch_size, num_workers = args
+    train_file, test_file, batch_size, epochs, gpu_mode, num_workers, retrain_model, \
+    retrain_model_path, gru_layers, hidden_size, learning_rate, weight_decay, model_dir, stats_dir, total_callers, \
+    train_mode = args
 
     # issue with semaphore lock: https://github.com/pytorch/pytorch/issues/2517
     # mp.set_start_method('spawn')
@@ -232,31 +263,20 @@ def setup(rank, device_ids, args, all_input_files):
     # Explicitly setting seed to make sure that models created in two processes
     # start from same random weights and biases. https://github.com/pytorch/pytorch/issues/2517
     torch.manual_seed(42)
-    predict(filepath,
-            all_input_files[rank],
-            output_filepath,
-            model_path,
-            batch_size,
-            num_workers,
-            rank,
-            device_ids[rank])
+    train(train_file, test_file, batch_size, epochs, gpu_mode, num_workers, retrain_model, retrain_model_path,
+          gru_layers, hidden_size, learning_rate, weight_decay, model_dir, stats_dir, train_mode,
+          total_callers, rank, device_ids[rank])
     cleanup()
 
 
-def train_distributed(filepath, file_chunks, output_filepath, model_path, batch_size, device_ids, num_workers):
-    """
-    Create a prediction table/dictionary of an images set using a trained model.
-    :param filepath: Path to image files to predict on
-    :param file_chunks: Path to chunked files
-    :param batch_size: Batch size used for prediction
-    :param model_path: Path to a trained model
-    :param output_filepath: Path to output directory
-    :param device_ids: List of GPU devices to use
-    :param num_workers: Number of workers to be used by the dataloader
-    :return: Prediction dictionary
-    """
-    args = (filepath, output_filepath, model_path, batch_size, num_workers)
+def train_distributed(train_file, test_file, batch_size, epochs, gpu_mode, num_workers, retrain_model,
+                          retrain_model_path, gru_layers, hidden_size, learning_rate, weight_decay, model_dir,
+                          stats_dir, device_ids, total_callers, train_mode):
+
+    args = (train_file, test_file, batch_size, epochs, gpu_mode, num_workers, retrain_model,
+            retrain_model_path, gru_layers, hidden_size, learning_rate, weight_decay, model_dir,
+            stats_dir, total_callers, train_mode)
     mp.spawn(setup,
-             args=(device_ids, args, file_chunks),
+             args=(device_ids, args),
              nprocs=len(device_ids),
              join=True)
--- modules/python/train_models.py
+++ modules/python/train_models.py
@@ -90,23 +90,24 @@ class TrainModule:
         if total_callers == 0:
             sys.stderr.write(TextColor.RED + "ERROR: NO GPU AVAILABLE BUT GPU MODE IS SET\n" + TextColor.END)
             exit()
-        exit()
 
-        model, optimizer, stats_dictionary = train(self.train_file,
-                                                   self.test_file,
-                                                   self.batch_size,
-                                                   self.epochs,
-                                                   self.gpu_mode,
-                                                   self.num_workers,
-                                                   self.retrain_model,
-                                                   self.retrain_model_path,
-                                                   self.gru_layers,
-                                                   self.hidden_size,
-                                                   self.learning_rate,
-                                                   self.weight_decay,
-                                                   self.model_dir,
-                                                   self.stats_dir,
-                                                   train_mode=True)
+        model, optimizer, stats_dictionary = train_distributed(self.train_file,
+                                                               self.test_file,
+                                                               self.batch_size,
+                                                               self.epochs,
+                                                               self.gpu_mode,
+                                                               self.num_workers,
+                                                               self.retrain_model,
+                                                               self.retrain_model_path,
+                                                               self.gru_layers,
+                                                               self.hidden_size,
+                                                               self.learning_rate,
+                                                               self.weight_decay,
+                                                               self.model_dir,
+                                                               self.stats_dir,
+                                                               device_ids,
+                                                               total_callers,
+                                                               train_mode=True)
 
         return model, optimizer, stats_dictionary
 
