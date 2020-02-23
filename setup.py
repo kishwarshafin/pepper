2c7e028dd9a332d64dbaef73344e2b62286a70fb
Kishwar <kishwar.shafin@gmail.com>
2020-02-22 22:39:24 -0800

    test :fire:

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import os
 import re
 import sys
+import pathlib
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
+from setuptools.command.build_ext import build_ext as build_ext_orig
 from distutils.version import LooseVersion
 import subprocess
 from pathlib import Path
@@ -12,94 +13,136 @@ __author__ = 'Kishwar Shafin'
 __description__ = 'RNN based standalone assembly polisher.'
 
 
+# class CMakeExtension(Extension):
+#     def __init__(self, name):
+#         Extension.__init__(self, name, sources=[])
+#
+#
+# class CMakeBuild(build_ext):
+#     def run(self):
+#         try:
+#             out = subprocess.check_output(['cmake', '--version'])
+#             cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
+#                                                    out.decode()).group(1))
+#             print("CMAKE VERSION: ", cmake_version)
+#             if cmake_version < '3':
+#                 raise RuntimeError("CMake >= 3 is required.")
+#         except OSError:
+#             raise RuntimeError(
+#                 "CMake must be installed to build the following extensions: " +
+#                 ", ".join(e.name for e in self.extensions))
+#
+#         build_directory = os.path.abspath(self.build_temp)
+#
+#         cmake_args = [
+#             '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + build_directory,
+#             '-DPYTHON_EXECUTABLE=' + sys.executable
+#         ]
+#
+#         cfg = 'Debug' if self.debug else 'Release'
+#         build_args = ['--config', cfg]
+#
+#         cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+#
+#         # Assuming Makefiles
+#         build_args += ['--', '-j2']
+#
+#         self.build_args = build_args
+#
+#         env = os.environ.copy()
+#         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+#             env.get('CXXFLAGS', ''),
+#             self.distribution.get_version())
+#         if not os.path.exists(self.build_temp):
+#             os.makedirs(self.build_temp)
+#
+#         # CMakeLists.txt is in the same directory as this setup.py file
+#         cmake_list_dir = os.path.abspath(os.path.dirname(__file__))
+#         print('-'*10, 'Running CMake prepare', '-'*40)
+#         subprocess.check_call(['cmake', cmake_list_dir] + cmake_args,
+#                               cwd=self.build_temp, env=env)
+#
+#         print('-'*10, 'Building extensions', '-'*40)
+#         cmake_cmd = ['cmake', '--build', '.'] + self.build_args
+#         subprocess.check_call(cmake_cmd,
+#                               cwd=self.build_temp)
+#
+#         # Move from build temp to final position
+#         for ext in self.extensions:
+#             print("MOVING EXT: ", ext)
+#             self.move_output(ext)
+#
+#     def move_output(self, ext):
+#         print("IN MOVE")
+#         build_temp = Path(self.build_temp).resolve()
+#         print("1", str(build_temp))
+#         dest_path = Path(self.get_ext_fullpath(ext.name)).resolve()
+#         print("2", str(dest_path))
+#         source_path = build_temp / self.get_ext_filename(ext.name)
+#         print("3", str(source_path))
+#         dest_directory = dest_path.parents[0]
+#         print("4", str(dest_directory))
+#         dest_directory.mkdir(parents=True, exist_ok=True)
+#         self.copy_file(source_path, dest_path)
+#
+#     def move_output_new(self, ext):
+#         print("IN MOVE")
+#         build_temp = Path(self.build_temp).resolve()
+#         print("1", build_temp)
+#         print("1.1", str(Path(self.get_ext_fullpath(ext.name)).resolve().parents[0]))
+#         print("1.2", self.get_ext_filename(ext.name))
+#         dest_path = Path(self.get_ext_fullpath(ext.name)).resolve().parents[0] / "build" / self.get_ext_filename(ext.name)
+#         print("2", str(dest_path))
+#         source_path = build_temp / self.get_ext_filename(ext.name)
+#         print("3", str(source_path))
+#         dest_directory = dest_path.parents[0]
+#         print("4", str(dest_directory))
+#         dest_directory.mkdir(parents=True, exist_ok=True)
+#         print("SOURCE: ", source_path)
+#         print("DEST: ", dest_path)
+#         self.copy_file(source_path, dest_path)
+
 class CMakeExtension(Extension):
+
     def __init__(self, name):
-        Extension.__init__(self, name, sources=[])
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
 
 
-class CMakeBuild(build_ext):
+class build_ext(build_ext_orig):
     def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-                                                   out.decode()).group(1))
-            print("CMAKE VERSION: ", cmake_version)
-            if cmake_version < '3':
-                raise RuntimeError("CMake >= 3 is required.")
-        except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build the following extensions: " +
-                ", ".join(e.name for e in self.extensions))
+        for ext in self.extensions:
+            self.build_cmake(ext)
+        super().run()
 
-        build_directory = os.path.abspath(self.build_temp)
+    def build_cmake(self, ext):
+        cwd = pathlib.Path().absolute()
 
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = pathlib.Path(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extdir.mkdir(parents=True, exist_ok=True)
+
+        # example of cmake args
+        config = 'Debug' if self.debug else 'Release'
         cmake_args = [
-            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + build_directory,
-            '-DPYTHON_EXECUTABLE=' + sys.executable
+            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + str(extdir.parent.absolute()),
+            '-DCMAKE_BUILD_TYPE=' + config
         ]
 
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
+        # example of build args
+        build_args = [
+            '--config', config,
+            '--', '-j4'
+        ]
 
-        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-
-        # Assuming Makefiles
-        build_args += ['--', '-j2']
-
-        self.build_args = build_args
-
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get('CXXFLAGS', ''),
-            self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-
-        # CMakeLists.txt is in the same directory as this setup.py file
-        cmake_list_dir = os.path.abspath(os.path.dirname(__file__))
-        print('-'*10, 'Running CMake prepare', '-'*40)
-        subprocess.check_call(['cmake', cmake_list_dir] + cmake_args,
-                              cwd=self.build_temp, env=env)
-
-        print('-'*10, 'Building extensions', '-'*40)
-        cmake_cmd = ['cmake', '--build', '.'] + self.build_args
-        subprocess.check_call(cmake_cmd,
-                              cwd=self.build_temp)
-
-        # Move from build temp to final position
-        for ext in self.extensions:
-            print("MOVING EXT: ", ext)
-            self.move_output(ext)
-
-    def move_output(self, ext):
-        print("IN MOVE")
-        build_temp = Path(self.build_temp).resolve()
-        print("1", str(build_temp))
-        dest_path = Path(self.get_ext_fullpath(ext.name)).resolve()
-        print("2", str(dest_path))
-        source_path = build_temp / self.get_ext_filename(ext.name)
-        print("3", str(source_path))
-        dest_directory = dest_path.parents[0]
-        print("4", str(dest_directory))
-        dest_directory.mkdir(parents=True, exist_ok=True)
-        self.copy_file(source_path, dest_path)
-
-    def move_output_new(self, ext):
-        print("IN MOVE")
-        build_temp = Path(self.build_temp).resolve()
-        print("1", build_temp)
-        print("1.1", str(Path(self.get_ext_fullpath(ext.name)).resolve().parents[0]))
-        print("1.2", self.get_ext_filename(ext.name))
-        dest_path = Path(self.get_ext_fullpath(ext.name)).resolve().parents[0] / "build" / self.get_ext_filename(ext.name)
-        print("2", str(dest_path))
-        source_path = build_temp / self.get_ext_filename(ext.name)
-        print("3", str(source_path))
-        dest_directory = dest_path.parents[0]
-        print("4", str(dest_directory))
-        dest_directory.mkdir(parents=True, exist_ok=True)
-        print("SOURCE: ", source_path)
-        print("DEST: ", dest_path)
-        self.copy_file(source_path, dest_path)
+        os.chdir(str(build_temp))
+        self.spawn(['cmake', str(cwd)] + cmake_args)
+        if not self.dry_run:
+            self.spawn(['cmake', '--build', '.'] + build_args)
+        os.chdir(str(cwd))
 
 
 def get_dependencies():
