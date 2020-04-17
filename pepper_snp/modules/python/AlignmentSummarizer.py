1f41d38995b989475b5f769ebd1e3b96d154c93f
Kishwar <kishwar.shafin@gmail.com>
2020-04-16 18:00:23 -0700

    intersection message removed :fire:

--- pepper_snp/modules/python/AlignmentSummarizer.py
+++ pepper_snp/modules/python/AlignmentSummarizer.py
@@ -188,7 +188,7 @@ class AlignmentSummarizer:
         found_overlap = False
         for i in range(1, len(intervals)):
             if intervals[i][0] > right or intervals[i][1] < left:
-                print(-1)
+                continue
             else:
                 found_overlap = True
                 left = max(left, intervals[i][0])
