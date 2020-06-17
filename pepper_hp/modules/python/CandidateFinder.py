08eb33b07cbe6c919605102ae067ce4439d332fb
Kishwar <kishwar.shafin@gmail.com>
2020-06-17 13:09:00 -0700

    v6 freq thresholds back for indels :fire:

--- pepper_hp/modules/python/CandidateFinder.py
+++ pepper_hp/modules/python/CandidateFinder.py
@@ -294,8 +294,8 @@ def filter_candidate(candidate_type, depth, read_support, read_support_h0, read_
         #     return True
         # if max(non_ref_prob, alt_prob_h1, alt_prob_h2) >= CandidateFinderOptions.IN_LAST_CHANCE_THRESHOLD:
         #     return True
-        # if allele_frequency >= CandidateFinderOptions.IN_FREQ_THRESHOLD:
-        #     return True
+        if allele_frequency >= CandidateFinderOptions.IN_FREQ_THRESHOLD:
+            return True
 
     # delete alleles
     elif candidate_type == 3:
@@ -305,8 +305,8 @@ def filter_candidate(candidate_type, depth, read_support, read_support_h0, read_
         #     return True
         # if max(non_ref_prob, alt_prob_h1, alt_prob_h2) >= CandidateFinderOptions.DEL_LAST_CHANCE_THRESHOLD:
         #     return True
-        # if allele_frequency >= CandidateFinderOptions.DEL_FREQ_THRESHOLD:
-        #     return True
+        if allele_frequency >= CandidateFinderOptions.DEL_FREQ_THRESHOLD:
+            return True
 
     # otherwise, it's highly unlikely to be a true variant.
     return False
