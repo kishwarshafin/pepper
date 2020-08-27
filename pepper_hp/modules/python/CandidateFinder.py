4fb59af58691999a2fbaa0ab5b6c921a9b6d2dba
Kishwar <kishwar.shafin@gmail.com>
2020-08-27 16:05:22 -0700

    lower frequency thresholds added for indel :fire:

--- pepper_hp/modules/python/CandidateFinder.py
+++ pepper_hp/modules/python/CandidateFinder.py
@@ -299,6 +299,9 @@ def filter_candidate(candidate_type, depth, read_support, read_support_h0, read_
         allele_weight = max(alt_prob_h1, alt_prob_h2)
 
         if allele_frequency <= CandidateFinderOptions.IN_FREQ_THRESHOLD:
+            if allele_frequency < CandidateFinderOptions.IN_FREQ_LOWER_THRESHOLD:
+                return False
+
             if allele_weight >= 0.5 or non_ref_prob >= 0.5:
                 return True
             else:
@@ -320,6 +323,9 @@ def filter_candidate(candidate_type, depth, read_support, read_support_h0, read_
         allele_weight = max(alt_prob_h1, alt_prob_h2)
 
         if allele_frequency <= CandidateFinderOptions.DEL_FREQ_THRESHOLD:
+            if allele_frequency < CandidateFinderOptions.DEL_FREQ_LOWER_THRESHOLD:
+                return False
+
             if allele_weight >= 0.5 or non_ref_prob >= 0.5:
                 return True
             else:
