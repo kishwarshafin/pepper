e1fa05aa3d62554a464ad7479ab8f371149dab29
Kishwar <kishwar.shafin@gmail.com>
2020-10-07 12:18:14 -0700

    multi-processing implemented :fire:

--- pepper_hp/modules/python/models/predict_distributed_gpu.py
+++ pepper_hp/modules/python/models/predict_distributed_gpu.py
@@ -6,14 +6,17 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel
+import time
 from datetime import datetime
+import concurrent.futures
 
 from pepper_hp.modules.python.models.dataloader_predict import SequenceDataset
 from pepper_hp.modules.python.models.ModelHander import ModelHandler
 from pepper_hp.modules.python.Options import ImageSizeOptions, TrainOptions
 from pepper_hp.modules.python.DataStorePredict import DataStore
-os.environ['PYTHONWARNINGS'] = 'ignore:semaphore_tracker:UserWarning'
-torch.multiprocessing.set_sharing_strategy('file_system')
+#os.environ['PYTHONWARNINGS'] = 'ignore:semaphore_tracker:UserWarning'
+#torch.multiprocessing.set_sharing_strategy('file_system')
+
 
 def predict(input_filepath, file_chunks, output_filepath, model_path, batch_size, num_workers, theads_per_caller, device_id, rank):
     transducer_model, hidden_size, gru_layers, prev_ite = \
@@ -113,31 +116,52 @@ def predict(input_filepath, file_chunks, output_filepath, model_path, batch_size
 
 
 
-def cleanup():
-    dist.destroy_process_group()
+# def cleanup():
+#     dist.destroy_process_group()
+#
+#
+# def setup(rank, total_callers, args, all_input_files):
+#     os.environ['MASTER_ADDR'] = 'localhost'
+#     os.environ['MASTER_PORT'] = '12355'
+#
+#     # initialize the process group
+#     dist.init_process_group("gloo", rank=rank, world_size=total_callers)
+#
+#     filepath, output_filepath, model_path, batch_size, threads_per_caller, device_ids, num_workers = args
+#
+#     # issue with semaphore lock: https://github.com/pytorch/pytorch/issues/2517
+#     # mp.set_start_method('spawn')
+#
+#     # Explicitly setting seed to make sure that models created in two processes
+#     # start from same random weights and biases. https://github.com/pytorch/pytorch/issues/2517
+#     # torch.manual_seed(42)
+#
+#     predict(filepath, all_input_files[rank],  output_filepath, model_path, batch_size, num_workers, threads_per_caller, device_ids[rank], rank)
+#     cleanup()
 
 
-def setup(rank, total_callers, args, all_input_files):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+# def predict_distributed_gpu(filepath, file_chunks, output_filepath, model_path, batch_size, total_callers, threads_per_caller, device_ids, num_workers):
+#     """
+#     Create a prediction table/dictionary of an images set using a trained model.
+#     :param filepath: Path to image files to predict on
+#     :param file_chunks: Path to chunked files
+#     :param batch_size: Batch size used for prediction
+#     :param model_path: Path to a trained model
+#     :param output_filepath: Path to output directory
+#     :param total_callers: Number of callers
+#     :param threads_per_caller: How many threads to set per caller
+#     :param device_ids: Device ID of GPU to be used
+#     :param num_workers: Number of workers to be used by the dataloader
+#     :return: Prediction dictionary
+#     """
+#     args = (filepath, output_filepath, model_path, batch_size, threads_per_caller, device_ids, num_workers)
+#     mp.spawn(setup,
+#              args=(total_callers, args, file_chunks),
+#              nprocs=total_callers,
+#              join=True)
 
-    # initialize the process group
-    dist.init_process_group("gloo", rank=rank, world_size=total_callers)
 
-    filepath, output_filepath, model_path, batch_size, threads_per_caller, device_ids, num_workers = args
-
-    # issue with semaphore lock: https://github.com/pytorch/pytorch/issues/2517
-    # mp.set_start_method('spawn')
-
-    # Explicitly setting seed to make sure that models created in two processes
-    # start from same random weights and biases. https://github.com/pytorch/pytorch/issues/2517
-    # torch.manual_seed(42)
-
-    predict(filepath, all_input_files[rank],  output_filepath, model_path, batch_size, num_workers, threads_per_caller, device_ids[rank], rank)
-    cleanup()
-
-
-def predict_distributed_gpu(filepath, file_chunks, output_filepath, model_path, batch_size, total_callers, threads_per_caller, device_ids, num_workers):
+def predict_distributed_gpu(filepath, all_input_files, output_filepath, model_path, batch_size, total_callers, threads_per_caller, device_ids, num_workers):
     """
     Create a prediction table/dictionary of an images set using a trained model.
     :param filepath: Path to image files to predict on
@@ -151,8 +175,23 @@ def predict_distributed_gpu(filepath, file_chunks, output_filepath, model_path, 
     :param num_workers: Number of workers to be used by the dataloader
     :return: Prediction dictionary
     """
-    args = (filepath, output_filepath, model_path, batch_size, threads_per_caller, device_ids, num_workers)
-    mp.spawn(setup,
-             args=(total_callers, args, file_chunks),
-             nprocs=total_callers,
-             join=True)
+    start_time = time.time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=total_callers) as executor:
+        futures = [executor.submit(predict, filepath, all_input_files[rank],  output_filepath, model_path, batch_size, num_workers, threads_per_caller, device_ids[rank], rank)
+                   for rank in range(0, total_callers)]
+
+        for fut in concurrent.futures.as_completed(futures):
+            if fut.exception() is None:
+                # get the results
+                thread_id = fut.result()
+                sys.stderr.write("[" + str(datetime.now().strftime('%m-%d-%Y %H:%M:%S')) + "] INFO: CALLER "
+                                 + str(thread_id) + " FINISHED SUCCESSFULLY.\n")
+            else:
+                sys.stderr.write("ERROR: " + str(fut.exception()) + "\n")
+            fut._result = None  # python issue 27144
+
+    end_time = time.time()
+    mins = int((end_time - start_time) / 60)
+    secs = int((end_time - start_time)) % 60
+    sys.stderr.write("[" + str(datetime.now().strftime('%m-%d-%Y %H:%M:%S')) + "] INFO: FINISHED INFERENCE.\n")
+    sys.stderr.write("[" + str(datetime.now().strftime('%m-%d-%Y %H:%M:%S')) + "] INFO: ELAPSED TIME: " + str(mins) + " Min " + str(secs) + " Sec\n")
