98c927b5e1dac3f5b7920589a9e28fc6f70c1073
Kishwar <kishwar.shafin@gmail.com>
2021-03-30 11:36:19 -0700

    prediction updated :fire:

--- pepper_variant/modules/python/models/predict_distributed_gpu.py
+++ pepper_variant/modules/python/models/predict_distributed_gpu.py
@@ -43,32 +43,52 @@ def predict(input_filepath, file_chunks, output_filepath, model_path, batch_size
 
     batch_completed = 0
     total_batches = len(data_loader)
+
     with torch.no_grad():
         for contig, contig_start, contig_end, chunk_id, images, position, index in data_loader:
             sys.stderr.flush()
             images = images.type(torch.FloatTensor)
             hidden = torch.zeros(images.size(0), 2 * TrainOptions.GRU_LAYERS, TrainOptions.HIDDEN_SIZE)
+            prediction_base_tensor = torch.zeros((images.size(0), images.size(1), ImageSizeOptions.TOTAL_LABELS))
 
             images = images.to(device_id)
             hidden = hidden.to(device_id)
+            prediction_base_tensor = prediction_base_tensor.to(device_id)
 
-            # run inference
-            output_base, hidden = transducer_model(images, hidden)
+            for i in range(0, ImageSizeOptions.SEQ_LENGTH, TrainOptions.WINDOW_JUMP):
+                if i + TrainOptions.TRAIN_WINDOW > ImageSizeOptions.SEQ_LENGTH:
+                    break
+                chunk_start = i
+                chunk_end = i + TrainOptions.TRAIN_WINDOW
+                # chunk all the data
+                image_chunk = images[:, chunk_start:chunk_end]
 
-            # do softmax and get prediction
-            # we run a softmax a padding to make the output tensor compatible for adding
-            inference_layers = nn.Sequential(
+                # run inference
+                output_base, hidden = transducer_model(image_chunk, hidden)
+
+                # now calculate how much padding is on the top and bottom of this chunk so we can do a simple
+                # add operation
+                top_zeros = chunk_start
+                bottom_zeros = ImageSizeOptions.SEQ_LENGTH - chunk_end
+
+                # do softmax and get prediction
+                # we run a softmax a padding to make the output tensor compatible for adding
+                inference_layers = nn.Sequential(
                     nn.Softmax(dim=2),
-            )
-            inference_layers = inference_layers.to(device_id)
+                    nn.ZeroPad2d((0, 0, top_zeros, bottom_zeros))
+                )
+                inference_layers = inference_layers.to(device_id)
 
-            # run the softmax and padding layers
-            base_prediction = (inference_layers(output_base) * 1000000).type(torch.IntTensor).to(device_id)
+                # run the softmax and padding layers
+                base_prediction = (inference_layers(output_base) * 100000).type(torch.IntTensor).to(device_id)
 
-            del inference_layers
-            torch.cuda.empty_cache()
+                # now simply add the tensor to the global counter
+                prediction_base_tensor = torch.add(prediction_base_tensor, base_prediction)
 
-            base_prediction = base_prediction.cpu().numpy().astype(int)
+                del inference_layers
+                torch.cuda.empty_cache()
+
+            prediction_base_tensor = prediction_base_tensor.cpu().numpy().astype(int)
 
             for i in range(images.size(0)):
                 prediction_data_file.write_prediction(contig[i],
@@ -77,7 +97,7 @@ def predict(input_filepath, file_chunks, output_filepath, model_path, batch_size
                                                       chunk_id[i],
                                                       position[i],
                                                       index[i],
-                                                      base_prediction[i])
+                                                      prediction_base_tensor[i])
             batch_completed += 1
 
             if rank == 0 and batch_completed % 5 == 0:
