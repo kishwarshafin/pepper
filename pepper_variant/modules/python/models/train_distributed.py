0ac13295a5c3e546692363b24f893bf0d3e11998
Kishwar <kishwar.shafin@gmail.com>
2021-05-18 15:09:42 -0700

    type removed

--- pepper_variant/modules/python/models/train_distributed.py
+++ pepper_variant/modules/python/models/train_distributed.py
@@ -177,7 +177,8 @@ def train(train_file, test_file, batch_size, epoch_limit, gpu_mode, num_workers,
 
             model_optimizer.zero_grad()
 
-            output_base, output_type = transducer_model(images, hidden, cell_state, train_mode)
+            output_base = transducer_model(images, hidden, cell_state, train_mode)
+            # output_base, output_type = transducer_model(images, hidden, cell_state, train_mode)
 
             loss_base = criterion_base(output_base.contiguous().view(-1, num_classes), labels.contiguous().view(-1))
             # loss_type = criterion_type(output_type.contiguous().view(-1, num_type_classes), type_labels.contiguous().view(-1))
