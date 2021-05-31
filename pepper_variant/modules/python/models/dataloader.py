c6ee3a2c9c9d789ae51a173a4d0294b501f93f29
Kishwar <kishwar.shafin@gmail.com>
2021-05-31 11:03:22 -0700

    data loader update

--- pepper_variant/modules/python/models/dataloader.py
+++ pepper_variant/modules/python/models/dataloader.py
@@ -3,6 +3,7 @@ from os import listdir
 from torch.utils.data import Dataset
 from pepper_variant.modules.python.Options import ImageSizeOptions
 import torchvision.transforms as transforms
+from collections import defaultdict
 import h5py
 import sys
 import numpy as np
@@ -26,25 +27,42 @@ class SequenceDataset(Dataset):
     """
     def __init__(self, image_directory):
         self.transform = transforms.Compose([transforms.ToTensor()])
+        self.hdf_filenames = defaultdict()
+        self.region_names = defaultdict()
         file_image_pair = []
 
         hdf_files = get_file_paths_from_directory(image_directory)
-
+        hdf5_index = 1
+        region_index = 1
         for hdf5_file_path in hdf_files:
+            # index hdf5_file_path
+            if hdf5_file_path not in self.hdf_filenames:
+                self.hdf_filenames[hdf5_file_path] = hdf5_index
+                hdf5_index += 1
+
             with h5py.File(hdf5_file_path, 'r') as hdf5_file:
                 if 'summaries' in hdf5_file:
                     region_names = list(hdf5_file['summaries'].keys())
+
                     for region_name in region_names:
+                        # index region names
+                        if region_name not in self.hdf_filenames.keys():
+                            self.region_names[region_name] = region_index
+                            region_index += 1
+
                         image_shape = hdf5_file['summaries'][region_name]['images'].shape[0]
 
                         for index in range(0, image_shape):
-                            file_image_pair.append((hdf5_file_path, region_name, index))
+                            file_image_pair.append((self.hdf_filenames[hdf5_file_path], self.region_names[region_name], index))
 
         self.all_images = file_image_pair
 
     def __getitem__(self, index):
         # load the image
-        hdf5_filepath, region_name, indx = self.all_images[index]
+        hdf5_filepath_index, region_name_index, indx = self.all_images[index]
+
+        hdf5_filepath = self.hdf_filenames[hdf5_filepath_index]
+        region_name = self.region_names[hdf5_filepath_index]
 
         with h5py.File(hdf5_filepath, 'r') as hdf5_file:
             image = hdf5_file['summaries'][region_name]['images'][indx][()]
