66781652c2c68acf62508c846a4dc8482de69241
Kishwar <kishwar.shafin@gmail.com>
2021-06-18 10:23:20 -0700

    debug

--- pepper_variant/modules/python/models/predict_distributed_cpu.py
+++ pepper_variant/modules/python/models/predict_distributed_cpu.py
@@ -51,6 +51,7 @@ def predict(input_filepath, file_chunks, output_filepath, model_path, batch_size
     total_batches = len(data_loader)
     with torch.no_grad():
         for contigs, positions, depths, candidates, candidate_frequencies, images in data_loader:
+            print("HERE")
             sys.stderr.flush()
             images = images.type(torch.FloatTensor)
             hidden = torch.zeros(images.size(0), 2 * TrainOptions.GRU_LAYERS, TrainOptions.HIDDEN_SIZE)
