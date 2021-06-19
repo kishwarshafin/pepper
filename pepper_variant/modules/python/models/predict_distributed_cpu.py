08dc482c517173c18cb7fc860d85ef37762adab9
Kishwar <kishwar.shafin@gmail.com>
2021-06-18 21:16:04 -0700

    parallel execution

--- pepper_variant/modules/python/models/predict_distributed_cpu.py
+++ pepper_variant/modules/python/models/predict_distributed_cpu.py
@@ -32,7 +32,8 @@ def predict(input_filepath, file_chunks, output_filepath, model_path, batch_size
 
     # session options
     sess_options = onnxruntime.SessionOptions()
-    sess_options.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
+    # sess_options.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
+    sess_options.execution_mode = onnxruntime.ExecutionMode.ORT_PARALLEL
     sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
     ort_session = onnxruntime.InferenceSession(model_path + "quantized.onnx", sess_options=sess_options)
 
@@ -40,8 +41,8 @@ def predict(input_filepath, file_chunks, output_filepath, model_path, batch_size
         sys.stderr.write("[" + str(datetime.now().strftime('%m-%d-%Y %H:%M:%S')) + "] " + "INFO: SETTING THREADS TO: " + str(threads) + ".\n")
         sys.stderr.flush()
 
-    sess_options.intra_op_num_threads = 1
-    torch.set_num_threads(1)
+    sess_options.intra_op_num_threads = threads
+    # torch.set_num_threads(1)
 
     if thread_id == 0:
         sys.stderr.write("[" + str(datetime.now().strftime('%m-%d-%Y %H:%M:%S')) + "] " + "INFO: STARTING INFERENCE." + "\n")
