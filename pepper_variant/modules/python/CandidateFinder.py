715d45d9051b25e112187ddb2d404649bcc80c6e
Kishwar <kishwar.shafin@gmail.com>
2021-08-18 12:15:14 -0700

    indel p to 0.3

--- pepper_variant/modules/python/CandidateFinder.py
+++ pepper_variant/modules/python/CandidateFinder.py
@@ -382,7 +382,7 @@ def small_chunk_stitch(reference_file_path, bam_file_path, use_hp_info, file_chu
             variant_allele_support = []
             max_delete_length = 0
             reference_allele = reference_base
-            max_indel_p_value = 0.4
+            max_indel_p_value = 0.3
             max_snp_p_value = 0.1
             for alt_allele, allele_frequency in zip(candidate.candidates, candidate.candidate_frequency):
                 if allele_frequency <= 2:
