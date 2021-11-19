057db1bf8ab9ed193dffc75fbb03a088894f81d3
Kishwar <kishwar.shafin@gmail.com>
2021-11-19 13:17:00 -0800

    ONT parameters changed

--- pepper_variant/modules/argparse/SetParameters.py
+++ pepper_variant/modules/argparse/SetParameters.py
@@ -42,13 +42,13 @@ def set_parameters(options):
             if options.snp_p_value is None:
                 options.snp_p_value = 0.2
             if options.insert_p_value is None:
-                options.insert_p_value = 0.25
+                options.insert_p_value = 0.3
             if options.delete_p_value is None:
-                options.delete_p_value = 0.25
+                options.delete_p_value = 0.3
             if options.snp_q_cutoff is None:
-                options.snp_q_cutoff = 30
+                options.snp_q_cutoff = 15
             if options.indel_q_cutoff is None:
-                options.indel_q_cutoff = 30
+                options.indel_q_cutoff = 15
     if options.ont_r10_q20:
         sys.stderr.write("[" + str(datetime.now().strftime('%m-%d-%Y %H:%M:%S')) + "] INFO: ONT VARIANT CALLING MODE SELECTED.\n")
         # image generation
