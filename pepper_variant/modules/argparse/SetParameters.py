462d7d4165a961e697c4e3952f58908ec4028b44
Kishwar <kishwar.shafin@gmail.com>
2022-02-12 11:22:20 -0800

    parameter update

--- pepper_variant/modules/argparse/SetParameters.py
+++ pepper_variant/modules/argparse/SetParameters.py
@@ -40,11 +40,11 @@ def set_parameters(options):
             if options.allowed_multiallelics is None:
                 options.allowed_multiallelics = 4
             if options.snp_p_value is None:
-                options.snp_p_value = 0.1
+                options.snp_p_value = 0.05
             if options.insert_p_value is None:
-                options.insert_p_value = 0.1
+                options.insert_p_value = 0.2
             if options.delete_p_value is None:
-                options.delete_p_value = 0.1
+                options.delete_p_value = 0.2
             if options.snp_q_cutoff is None:
                 options.snp_q_cutoff = 20
             if options.indel_q_cutoff is None:
@@ -54,11 +54,11 @@ def set_parameters(options):
             if options.report_indel_above_freq is None:
                 options.report_indel_above_freq = 0
             if options.snp_p_value_in_lc is None:
-                options.snp_p_value_in_lc = 0.05
+                options.snp_p_value_in_lc = 0.01
             if options.insert_p_value_in_lc is None:
-                options.insert_p_value_in_lc = 0.05
+                options.insert_p_value_in_lc = 0.2
             if options.delete_p_value_in_lc is None:
-                options.delete_p_value_in_lc = 0.05
+                options.delete_p_value_in_lc = 0.2
             if options.snp_q_cutoff_in_lc is None:
                 options.snp_q_cutoff_in_lc = 20
             if options.indel_q_cutoff_in_lc is None:
