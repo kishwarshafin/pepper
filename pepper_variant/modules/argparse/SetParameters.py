d7437c5e0765ed2671a5b71e93abfe9141633423
Kishwar <kishwar.shafin@gmail.com>
2022-03-15 08:20:57 -0700

    parameter update

--- pepper_variant/modules/argparse/SetParameters.py
+++ pepper_variant/modules/argparse/SetParameters.py
@@ -60,7 +60,7 @@ def set_parameters(options):
             if options.delete_p_value_in_lc is None:
                 options.delete_p_value_in_lc = 0.1
             if options.snp_q_cutoff_in_lc is None:
-                options.snp_q_cutoff_in_lc = 15
+                options.snp_q_cutoff_in_lc = 20
             if options.indel_q_cutoff_in_lc is None:
                 options.indel_q_cutoff_in_lc = 10
 
